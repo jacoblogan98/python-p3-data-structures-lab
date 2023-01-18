55f54e25084f5cb62298613caf874ad68a5e4285
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2023-01-17 22:37:24 -0500

    replaced obj with food

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,23 +17,23 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    return [obj["name"] for obj in spicy_foods]
+    return [food["name"] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    return [obj for obj in spicy_foods if obj["heat_level"] > 5]
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
-    for obj in spicy_foods:
-        print(obj["name"] + " (" + obj["cuisine"] + ") | Heat Level: " + '🌶' * obj["heat_level"])
+    for food in spicy_foods:
+        print(food["name"] + " (" + food["cuisine"] + ") | Heat Level: " + '🌶' * food["heat_level"])
 
 def create_spicy_food(spicy_foods, spicy_food):
     spicy_foods.append(spicy_food)
     return spicy_foods
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    for obj in spicy_foods:
-        if obj["cuisine"] == cuisine:
-            return obj
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
 
 def print_spiciest_foods(spicy_foods):
     print_spicy_foods(get_spiciest_foods(spicy_foods))
